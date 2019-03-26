eddde7248478231506d86c834f51093584c4d544
plebanm <pleban.marcin@gmail.com>
2019-03-26 11:51:48 +0100

    create page where user choose weapon

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,24 @@
-function createPlayer(name, weapon = undefined) {
+function createPlayer() {
+    localStorage.clear();
+    // let name = document.getElementById('login').value;
     let player = {
-        name: name,
-        weapon: weapon
+        name: document.getElementById('login').value,
+        weapon: undefined
     };
-    return weapon;
+    console.log(player.name);
+    localStorage.setItem('userData', JSON.stringify(player));
+}
+
+function showUserName() {
+    let storageData = JSON.parse(localStorage.getItem('userData'));
+    console.log(storageData);
+    document.getElementById('showName').innerHTML += storageData['name'];
+}
+
+function addWeapon(weapon) {
+    let player = JSON.parse(localStorage.getItem('userData'));
+    player["weapon"]=weapon;
+    console.log(player.weapon);
+    localStorage.setItem('userData', JSON.stringify(player));
+
 }
