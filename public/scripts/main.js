dff6349dfd2591fe65ab6c41e2916d9bc45934de
Marcin Pleban <pleban.marcin@gmail.com>
2019-03-26 18:24:59 +0100

    Revert "Choose weapon"

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,26 +11,3 @@ readData.on('value', function (snap) {
 
 
 
-function createPlayer() {
-    localStorage.clear();
-    let player = {
-        name: document.getElementById('login').value,
-        weapon: undefined
-    };
-    console.log(player.name);
-    localStorage.setItem('userData', JSON.stringify(player));
-}
-
-function showUserName() {
-    let storageData = JSON.parse(localStorage.getItem('userData'));
-    console.log(storageData);
-    document.getElementById('showName').innerHTML += storageData['name'];
-}
-
-function addWeapon(weapon) {
-    let player = JSON.parse(localStorage.getItem('userData'));
-    player["weapon"]=weapon;
-    console.log(player.weapon);
-    localStorage.setItem('userData', JSON.stringify(player));
-
-}
